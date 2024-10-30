7b1f52af6dbdd546ee3c8c9595d68a4a0ae12a54
Tasfaynesh Tolasa <tasfayneshtolasa35@gmail.com>
2024-10-30 03:41:01 +0300

    Update AddEditNotes.jsx

--- client/src/pages/Home/AddEditNotes.jsx
+++ client/src/pages/Home/AddEditNotes.jsx
@@ -16,7 +16,7 @@ const AddEditNotes = ({ onClose, noteData, type, getAllNotes }) => {
 
     try {
       const res = await axios.post(
-        "http://localhost:3000/api/note/edit/" + noteId,
+        "https://api-note-app.vercel.app/api/note/edit/" + noteId,
         { title, content, tags },
         { withCredentials: true }
       );
@@ -40,7 +40,7 @@ const AddEditNotes = ({ onClose, noteData, type, getAllNotes }) => {
   const addNewNote = async () => {
     try {
       const res = await axios.post(
-        "http://localhost:3000/api/note/add",
+        "https://api-note-app.vercel.app/api/note/add",
         { title, content, tags },
         { withCredentials: true }
       );
